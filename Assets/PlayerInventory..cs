8928d44d8b689cc9baf9213304a67ef9eef2be88
Nafeesa Tanoli <nafeesatanoli252@gmail.com>
2026-07-06 15:41:00 +0500

    Scripting

--- Assets/PlayerInventory..cs
+++ Assets/PlayerInventory..cs
@@ -1,44 +1,6 @@
 using UnityEngine;
 
-public class DoorUnlock : MonoBehaviour
+public class PlayerInventory : MonoBehaviour
 {
-    public GameObject pressQUI;
-    public Animator doorAnimator;
-
-    bool playerNear = false;
-
-    void Start()
-    {
-        pressQUI.SetActive(false);
-    }
-
-    void Update()
-    {
-        if(playerNear && PlayerInventory.hasKey && Input.GetKeyDown(KeyCode.Q))
-        {
-            doorAnimator.SetTrigger("Open");
-
-            pressQUI.SetActive(false);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            playerNear = true;
-
-            if(PlayerInventory.hasKey)
-                pressQUI.SetActive(true);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            playerNear = false;
-            pressQUI.SetActive(false);
-        }
-    }
+    public static bool hasKey = false;
 }
